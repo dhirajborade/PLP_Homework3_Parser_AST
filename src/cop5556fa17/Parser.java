c278d6eebaf22c74c5ad7a0c0e4e6dc088d71806
Dhiraj Borade <dhirajborade@gmail.com>
2017-10-12 03:02:39 -0400

    Assignment Statement

--- src/cop5556fa17/Parser.java
+++ src/cop5556fa17/Parser.java
@@ -4,6 +4,8 @@ import cop5556fa17.Scanner.Kind;
 import cop5556fa17.Scanner.Token;
 import cop5556fa17.AST.ASTNode;
 import cop5556fa17.AST.Declaration;
+import cop5556fa17.AST.Declaration_Image;
+import cop5556fa17.AST.Declaration_SourceSink;
 import cop5556fa17.AST.Declaration_Variable;
 import cop5556fa17.AST.Expression;
 import cop5556fa17.AST.LHS;
@@ -97,34 +99,69 @@ public class Parser {
 	}
 
 	Declaration declaration() throws SyntaxException {
-		Token firstToken;
-		Declaration dec = null;
 		switch (t.kind) {
 		case KW_int:
 		case KW_boolean:
-			firstToken = t;
-			variableDeclaration();
-			break;
+			return variableDeclaration();
 		case KW_image:
-			imageDeclaration();
-			break;
+			return imageDeclaration();
 		case KW_url:
 		case KW_file:
-			sourceSinkDeclaration();
-			break;
+			return sourceSinkDeclaration();
 		default:
 			throw new SyntaxException(t, "Illegal Declaration");
 		}
 	}
+	
+	Declaration_Variable variableDeclaration() throws SyntaxException {
+		Token firstToken = varType();
+		Token type = t;
+		Token name = null;
+		Expression e = null;
+		matchToken(IDENTIFIER);
+		if (t.kind == OP_ASSIGN) {
+			name = t;
+			matchToken(OP_ASSIGN);
+			e = expression();
+		}
+		return new Declaration_Variable(firstToken, type, name, e);
+	}
+	
+	Declaration_Image imageDeclaration() throws SyntaxException {
+		matchToken(KW_image);
+		if (t.kind == LSQUARE) {
+			matchToken(LSQUARE);
+			expression();
+			matchToken(COMMA);
+			expression();
+			matchToken(RSQUARE);
+		}
+		matchToken(IDENTIFIER);
+		if (t.kind == OP_LARROW) {
+			matchToken(OP_LARROW);
+			source();
+		}
+
+	}
+	
+	Declaration_SourceSink sourceSinkDeclaration() throws SyntaxException {
+		Token firstToken = sourceSinkType();
+		Token type = t;
+		matchToken(IDENTIFIER);
+		Token name = t;
+		matchToken(OP_ASSIGN);
+		Source source = source();
+		return new Declaration_SourceSink(firstToken, type, name, source);
+	}
 
 	Statement statement() throws SyntaxException {
 		if (t.kind == IDENTIFIER && scanner.peek().kind == OP_RARROW) {
-			imageOutStatement();
+			return imageOutStatement();
 		} else if (t.kind == IDENTIFIER && scanner.peek().kind == OP_LARROW) {
-			imageInStatement();
+			return imageInStatement();
 		} else if ((t.kind == IDENTIFIER && scanner.peek().kind == OP_ASSIGN)
 				|| (t.kind == IDENTIFIER && scanner.peek().kind == LSQUARE)) {
-			assignmentStatement();
+			return assignmentStatement();
 		} else {
 			throw new SyntaxException(t, "Illegal Statement");
 		}
@@ -198,60 +235,35 @@ public class Parser {
 		matchToken(KW_r, COMMA, KW_A);
 	}
 
-	Declaration_Variable variableDeclaration() throws SyntaxException {
-		varType();
-		matchToken(IDENTIFIER);
-		if (t.kind == OP_ASSIGN) {
-			matchToken(OP_ASSIGN);
-			Expressionexpression();
-			return new Declaration_Variable(firstToken, type, name, e);
-		}
-	}
+	
 
-	void varType() throws SyntaxException {
+	Token varType() throws SyntaxException {
+		Token firstToken;
 		switch (t.kind) {
 		case KW_boolean:
+			firstToken = t;
 			matchToken(KW_boolean);
-			break;
+			return firstToken;
 		case KW_int:
+			firstToken = t;
 			matchToken(KW_int);
-			break;
+			return firstToken;
 		default:
 			throw new SyntaxException(t, "Illegal varType");
 		}
 	}
 
-	void imageDeclaration() throws SyntaxException {
-		matchToken(KW_image);
-		if (t.kind == LSQUARE) {
-			matchToken(LSQUARE);
-			expression();
-			matchToken(COMMA);
-			expression();
-			matchToken(RSQUARE);
-		}
-		matchToken(IDENTIFIER);
-		if (t.kind == OP_LARROW) {
-			matchToken(OP_LARROW);
-			source();
-		}
-
-	}
-
-	void sourceSinkDeclaration() throws SyntaxException {
-		sourceSinkType();
-		matchToken(IDENTIFIER, OP_ASSIGN);
-		source();
-	}
-
-	void sourceSinkType() throws SyntaxException {
+	Token sourceSinkType() throws SyntaxException {
+		Token firstToken;
 		switch (t.kind) {
 		case KW_url:
+			firstToken = t;
 			matchToken(KW_url);
-			break;
+			return firstToken;
 		case KW_file:
+			firstToken = t;
 			matchToken(KW_file);
-			break;
+			return firstToken;
 		default:
 			throw new SyntaxException(t, "Illegal Source Sink Type");
 		}
@@ -280,10 +292,11 @@ public class Parser {
 	 * 
 	 * Our test cases may invoke this routine directly to support incremental
 	 * development.
+	 * @return 
 	 * 
 	 * @throws SyntaxException
 	 */
-	void expression() throws SyntaxException {
+	Expression expression() throws SyntaxException {
 		// TODO implement this.
 		switch (t.kind) {
 		case OP_PLUS:
